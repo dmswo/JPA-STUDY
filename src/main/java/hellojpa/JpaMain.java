7fb7fba6e2f245bc26a258005d47830bb9819a8c
dmswo106 <dmswo106@naver.com>
2021-04-25 21:15:19 +0900

    JPA단방향 연관관계 공부

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,25 +16,28 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member1 = new Member();
-            member1.setUsername("A");
 
-            Member member2 = new Member();
-            member2.setUsername("B");
+            //저장
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
 
-            Member member3 = new Member();
-            member3.setUsername("C");
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
+            em.persist(member);
 
-            System.out.println("============");
+            em.flush();
+            em.clear();
 
-            em.persist(member1);  //1, 51
-            em.persist(member2);  //MEM
-            em.persist(member3);  //MEM
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = findMember.getTeam();
+            System.out.println(findTeam.getName());
 
-            System.out.println("member1 = " + member1.getId());
-            System.out.println("member2 = " + member2.getId());
-            System.out.println("member3 = " + member3.getId());
-            System.out.println("============");
+            //
+            Team newTeam = em.find(Team.class, 1L);
+            member.setTeam(newTeam);
+
             tx.commit();
         } catch (Exception e){
             tx.rollback();
