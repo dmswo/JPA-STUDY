2acf210f69eff9f3e60373ead493450fd7da98d0
dmswo106 <dmswo106@naver.com>
2021-04-26 21:48:21 +0900

    JPA양방향 연관관계 공부

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -31,12 +31,12 @@ public class JpaMain {
             em.clear();
 
             Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam();
-            System.out.println(findTeam.getName());
+            List<Member> members = findMember.getTeam().getMembers();
 
-            //
-            Team newTeam = em.find(Team.class, 1L);
-            member.setTeam(newTeam);
+            for(Member m : members){
+                System.out.println("m = " + m.getUsername());
+            }
+
 
             tx.commit();
         } catch (Exception e){
