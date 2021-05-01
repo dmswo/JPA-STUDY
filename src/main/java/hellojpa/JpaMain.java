dddc9191e4d9007d0cb22d6804f6420f05226b19
dmswo106 <dmswo106@naver.com>
2021-05-01 21:20:36 +0900

    @MappedSuperclass 공부

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,18 +16,15 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Movie movie = new Movie();
-            movie.setDirector("aaaa");
-            movie.setActor("bbbb");
-            movie.setName("바람과함께사라지다");
-            movie.setPrice(10000);
+            Member member = new Member();
+            member.setUsername("user1");
+            member.setCreateBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
 
-            em.persist(movie);
+            em.persist(member);
+
             em.flush();
             em.clear();
-
-            Movie findMove = em.find(Movie.class, movie.getId());
-            System.out.println(findMove);
 
             tx.commit();
         } catch (Exception e){
