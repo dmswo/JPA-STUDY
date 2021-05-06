d36f0a9eb6daa5b06997b8f1630ee39e082660e6
dmswo106 <dmswo106@naver.com>
2021-05-07 01:48:00 +0900

    JPQL공부1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,37 +14,15 @@ public class JpaMain {
         tx.begin();
 
         try {
+            String qlString = "select m from Member m where m.username like '%kim%'";
+            List<Member> result = em.createQuery(
+                    qlString
+                    ,Member.class)
+                    .getResultList();
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("=============Start===========");
-            Member findMember = em.find(Member.class, member.getId());
-
-            //homeCity -> newCity
-//            findMember.getHomeAddress().setCity("newCity");
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-//
-//            //치킨 -> 한식
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-
-//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
-//            findMember.getAddressHistory().add(new Address("newCity", "street", "10000"));
-
+            for (Member member : result) {
+                System.out.println("member = " + member);
+            }
             tx.commit();
         } catch (Exception e){
             tx.rollback();
